6d39272d35a6de39f85cd86af6d1bd129f050145
cjschardt <colin.schardt@gmail.com>
2019-11-04 18:19:52 -0800

    moved FreeRTOS tasks to own header, created a header for global constants and datatypes

--- projects/End_Effector/build/build/lpc40xx/compiled/source/main.cpp.d
+++ projects/End_Effector/build/build/lpc40xx/compiled/source/main.cpp.d
@@ -1,109 +1,132 @@
 build/build/lpc40xx/compiled/source/main.cpp.o: source/main.cpp \
- source/PID.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/utility/stopwatch.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/utility/log.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/config.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/project_config.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/log_levels.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/utility/units.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/third_party/units/units.h \
- /home/colin/Robotics2020/SJSU-Dev2/library/utility/ansi_terminal_codes.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/utility/constexpr.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/utility/debug.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/newlib/newlib.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/utility/build_info.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/utility/macros.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/utility/time.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/utility/status.hpp \
- source/MotorController.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/utility/map.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/lpc40xx/gpio.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/gpio.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/interrupt.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/lpc40xx/pin.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/pin.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/L0_Platform/lpc40xx/LPC40xx.h \
- /home/colin/Robotics2020/SJSU-Dev2/library/utility/bit.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/utility/enum.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/cortex/interrupt.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/pwm.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/lpc40xx/uart.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/L0_Platform/lpc17xx/LPC17xx.h \
- /home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/lpc40xx/system_controller.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/system_controller.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/uart.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/lpc40xx/adc.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/adc.hpp \
- /home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/lpc40xx/pwm.hpp
+ /home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/lpc40xx/uart.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/L0_Platform/lpc40xx/LPC40xx.h \
+ /home/colin/Robotics2020/SeniorProject/library/L0_Platform/lpc17xx/LPC17xx.h \
+ /home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/lpc40xx/pin.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/pin.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/utility/bit.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/utility/enum.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/utility/macros.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/lpc40xx/system_controller.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/project_config.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/config.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/log_levels.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/utility/units.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/third_party/units/units.h \
+ /home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/system_controller.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/utility/build_info.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/utility/log.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/utility/ansi_terminal_codes.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/utility/constexpr.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/utility/debug.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/newlib/newlib.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/utility/time.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/utility/status.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/uart.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/lpc40xx/adc.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/adc.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/lpc40xx/gpio.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/gpio.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/interrupt.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/cortex/interrupt.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/lpc40xx/pwm.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/pwm.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/L2_HAL/actuators/servo/servo.hpp \
+ /home/colin/Robotics2020/SeniorProject/library/utility/map.hpp \
+ source/RTOS_Tasks.hpp source/constants.h \
+ /home/colin/Robotics2020/SeniorProject/library/third_party/FreeRTOS/Source/include/FreeRTOS.h \
+ /home/colin/Robotics2020/SeniorProject/library/L0_Platform/freertos/FreeRTOSConfig.h \
+ /home/colin/Robotics2020/SeniorProject/library/third_party/FreeRTOS/Source/include/projdefs.h \
+ /home/colin/Robotics2020/SeniorProject/library/third_party/FreeRTOS/Source/include/portable.h \
+ /home/colin/Robotics2020/SeniorProject/library/third_party/FreeRTOS/Source/include/deprecated_definitions.h \
+ /home/colin/Robotics2020/SeniorProject/library/third_party/FreeRTOS/Source/include/mpu_wrappers.h \
+ /home/colin/Robotics2020/SeniorProject/library/third_party/FreeRTOS/Source/include/task.h \
+ /home/colin/Robotics2020/SeniorProject/library/third_party/FreeRTOS/Source/include/list.h
 
-source/PID.hpp:
+/home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/lpc40xx/uart.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/utility/stopwatch.hpp:
+/home/colin/Robotics2020/SeniorProject/library/L0_Platform/lpc40xx/LPC40xx.h:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/utility/log.hpp:
+/home/colin/Robotics2020/SeniorProject/library/L0_Platform/lpc17xx/LPC17xx.h:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/config.hpp:
+/home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/lpc40xx/pin.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/project_config.hpp:
+/home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/pin.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/log_levels.hpp:
+/home/colin/Robotics2020/SeniorProject/library/utility/bit.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/utility/units.hpp:
+/home/colin/Robotics2020/SeniorProject/library/utility/enum.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/third_party/units/units.h:
+/home/colin/Robotics2020/SeniorProject/library/utility/macros.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/utility/ansi_terminal_codes.hpp:
+/home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/lpc40xx/system_controller.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/utility/constexpr.hpp:
+/home/colin/Robotics2020/SeniorProject/library/project_config.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/utility/debug.hpp:
+/home/colin/Robotics2020/SeniorProject/library/config.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/newlib/newlib.hpp:
+/home/colin/Robotics2020/SeniorProject/library/log_levels.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/utility/build_info.hpp:
+/home/colin/Robotics2020/SeniorProject/library/utility/units.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/utility/macros.hpp:
+/home/colin/Robotics2020/SeniorProject/library/third_party/units/units.h:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/utility/time.hpp:
+/home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/system_controller.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/utility/status.hpp:
+/home/colin/Robotics2020/SeniorProject/library/utility/build_info.hpp:
 
-source/MotorController.hpp:
+/home/colin/Robotics2020/SeniorProject/library/utility/log.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/utility/map.hpp:
+/home/colin/Robotics2020/SeniorProject/library/utility/ansi_terminal_codes.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/lpc40xx/gpio.hpp:
+/home/colin/Robotics2020/SeniorProject/library/utility/constexpr.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/gpio.hpp:
+/home/colin/Robotics2020/SeniorProject/library/utility/debug.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/interrupt.hpp:
+/home/colin/Robotics2020/SeniorProject/library/newlib/newlib.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/lpc40xx/pin.hpp:
+/home/colin/Robotics2020/SeniorProject/library/utility/time.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/pin.hpp:
+/home/colin/Robotics2020/SeniorProject/library/utility/status.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/L0_Platform/lpc40xx/LPC40xx.h:
+/home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/uart.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/utility/bit.hpp:
+/home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/lpc40xx/adc.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/utility/enum.hpp:
+/home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/adc.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/cortex/interrupt.hpp:
+/home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/lpc40xx/gpio.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/pwm.hpp:
+/home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/gpio.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/lpc40xx/uart.hpp:
+/home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/interrupt.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/L0_Platform/lpc17xx/LPC17xx.h:
+/home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/cortex/interrupt.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/lpc40xx/system_controller.hpp:
+/home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/lpc40xx/pwm.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/system_controller.hpp:
+/home/colin/Robotics2020/SeniorProject/library/L1_Peripheral/pwm.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/uart.hpp:
+/home/colin/Robotics2020/SeniorProject/library/L2_HAL/actuators/servo/servo.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/lpc40xx/adc.hpp:
+/home/colin/Robotics2020/SeniorProject/library/utility/map.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/adc.hpp:
+source/RTOS_Tasks.hpp:
 
-/home/colin/Robotics2020/SJSU-Dev2/library/L1_Peripheral/lpc40xx/pwm.hpp:
+source/constants.h:
+
+/home/colin/Robotics2020/SeniorProject/library/third_party/FreeRTOS/Source/include/FreeRTOS.h:
+
+/home/colin/Robotics2020/SeniorProject/library/L0_Platform/freertos/FreeRTOSConfig.h:
+
+/home/colin/Robotics2020/SeniorProject/library/third_party/FreeRTOS/Source/include/projdefs.h:
+
+/home/colin/Robotics2020/SeniorProject/library/third_party/FreeRTOS/Source/include/portable.h:
+
+/home/colin/Robotics2020/SeniorProject/library/third_party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+/home/colin/Robotics2020/SeniorProject/library/third_party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+/home/colin/Robotics2020/SeniorProject/library/third_party/FreeRTOS/Source/include/task.h:
+
+/home/colin/Robotics2020/SeniorProject/library/third_party/FreeRTOS/Source/include/list.h:
--- projects/End_Effector/source/main.cpp
+++ projects/End_Effector/source/main.cpp
@@ -9,110 +9,18 @@
 #include "utility/log.hpp"
 #include "utility/map.hpp"
 #include "utility/time.hpp"
+#include "RTOS_Tasks.hpp"
+#include "constants.h"
 
 #include "third_party/FreeRTOS/Source/include/FreeRTOS.h"
 #include "third_party/FreeRTOS/Source/include/task.h"
 
-// Constants for the Linear Actuator Motor Controllers
-constexpr units::frequency::hertz_t motor_controller_freq = 75_Hz;
-constexpr std::chrono::microseconds motor_controller_min_pulse = 1000us;
-constexpr std::chrono::microseconds motor_controller_max_pulse = 2000us;
-
-void xUartTask(void* p);
-void xLinearActuator(void* p);
-
-//Task Handle for Uart task
-TaskHandle_t xUartTaskHandle;
-//Task Handle for LinearActuator task
-TaskHandle_t xLinearActuatorHandle;
-//Task Handle for CurrentSensor task
-TaskHandle_t xCurrentSensorHandle;
-
-union vals
-{
-  float f;
-  uint32_t ui;
-}rec[2];
-
-uint8_t receive = 0;
-// Declare peripheral objects
-sjsu::lpc40xx::Uart uart2(sjsu::lpc40xx::Uart::Port::kUart2);
-  
-sjsu::lpc40xx::Pwm p2_0(sjsu::lpc40xx::Pwm::Channel::kPwm0);
-sjsu::lpc40xx::Pwm p2_1(sjsu::lpc40xx::Pwm::Channel::kPwm1);
-
-uint32_t LA_position = 0;
-
-
-sjsu::Servo linear_actuator1(p2_0);
-sjsu::Servo linear_actuator2(p2_1);
-
 int main()
 {
-  uart2.Initialize(38400);
-  LOG_INFO("uart initialized");
-
-  linear_actuator1.Initialize();
-  linear_actuator1.SetFrequency(motor_controller_freq);
-  linear_actuator1.SetPulseBounds(motor_controller_min_pulse, 
-                                 motor_controller_max_pulse);
-
-  linear_actuator2.Initialize();
-  linear_actuator2.SetFrequency(motor_controller_freq);
-  linear_actuator2.SetPulseBounds(motor_controller_min_pulse, 
-                                 motor_controller_max_pulse);
-  LOG_INFO("Motor controller initialized");
-
-  bool prev_sign = false;
-
-  xTaskCreate(xUartTask, "uart_task", 1024, NULL, tskIDLE_PRIORITY + 2, &xUartTaskHandle);
-  xTaskCreate(xLinearActuator, "linear_actuator_task", 1024, NULL, tskIDLE_PRIORITY + 1, &xLinearActuatorHandle);
+  paramsStruct pvParameters;
+  xTaskCreate(xUartTask, "uart_task", 1024, (void *) &pvParameters, tskIDLE_PRIORITY + 2, &xUartTaskHandle);
+  xTaskCreate(xLinearActuator, "linear_actuator_task", 1024, (void *) &pvParameters, tskIDLE_PRIORITY + 1, &xLinearActuatorHandle);
 
   vTaskStartScheduler();
 }
 
-//Task to send and recive data over UART
-void xUartTask(void* p)
-{
-    while(1)
-    {
-      // LA_position = adc2.Read();
-      // Receive a float (Glove data) over UART
-      for(size_t j = 0 ; j<2; j++)
-      {
-        for (size_t i = 0; i < 4; i++)
-        {
-          receive = uart2.Read();
-          rec[j].ui = (rec[j].ui << 8) | receive;
-        }
-        //LOG_INFO("Read value %f over UART", rec.f);
-      }
-      // Delay 100 ms
-      vTaskDelay(500);
-    }
-}
-
-//Task to control linear actuators
-void xLinearActuator(void* p)
-{
-    while (1)
-    {
-      // Run the recieved data through the PID algorithm
-      // Map the output from the PID controller to proper units for the LA 
-      int converted_output[2] = {(sjsu::Map(rec[0].f, 0.0f, 3.3f, 1000.0f, 2000.0f)) , (sjsu::Map(rec[1].f, 0.0f, 3.3f, 1000.0f, 2000.0f))};
-    
-    //  LOG_INFO("%f",converted_output);
-    
-      linear_actuator1.SetPulseWidthInMicroseconds(static_cast<std::chrono::microseconds>(converted_output[0]));
-
-      linear_actuator2.SetPulseWidthInMicroseconds(static_cast<std::chrono::microseconds>(converted_output[1]));
-      // Delay 100 ms
-      vTaskDelay(100);
-    }
-}
-
-// //Task to read current sensors
-// void xCurrentSensor(void* p)
-// {
-
-// }
